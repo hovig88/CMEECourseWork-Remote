84398fd4cb040799eaafe0186a3122d9ffaea238
Hovig Artinian <artinianhovig@gmail.com>
2019-12-03 11:20:08 +0000

    Added docstring to TestR.py

--- Week7/code/TestR.py
+++ Week7/code/TestR.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python3
+
+"""Test python script that runs TestR.R using the subprocess module"""
+
+__appname__ = 'TestR.py'
+__author__ = 'Hovig Artinian (ha819@imperial.ac.uk)'
+__version__ = '0.0.1'
+__license__ = 'None'
+
+## imports ##
 import subprocess
+
 subprocess.Popen("Rscript --verbose TestR.R > ../results/TestR.Rout 2> ../results/TestR_errFile.Rout", shell=True).wait()
