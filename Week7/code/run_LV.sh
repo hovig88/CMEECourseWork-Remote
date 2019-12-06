2b90090837418e6eabfd92e703e146368918fa4f
Hovig Artinian <artinianhovig@gmail.com>
2019-12-06 16:19:28 +0000

    Updated run_LV.sh script

--- Week7/code/run_LV.sh
+++ Week7/code/run_LV.sh
@@ -1,40 +1,43 @@
 #!/bin/bash 
-
-# Runs and profiles LV1.py, LV2.py, LV3.py, and LV4.py
+# Author: Hovig Artinian ha819@imperial.ac.uk
+# Script: run_Vectorize.sh
+# Desc: runs and profiles LV1.py, LV2.py, LV3.py, and LV4.py
+# Arguments: None
+# Date: Oct 2019
 
 # First, run the scripts
-echo "Running LV1.py..."
+echo -e "\nRunning LV1.py...\n"
 python3 LV1.py
 if [ $? ] # If script runs successfully, the stored value of $? will be 0, which makes the if statement TRUE
 then
-    echo -e "Script ran successfully!\n"
+    echo -e "\nScript ran successfully!\n"
 else
-    echo -e "Script failed to run :(\n"
+    echo -e "\nScript failed to run :(\n"
 fi
 
-echo "Running LV2.py..."
+echo -e "Running LV2.py...\n"
 python3 LV2.py
 if [ $? ]
 then
-    echo -e "Script ran successfully!\n"
+    echo -e "\nScript ran successfully!\n"
 else
-    echo -e "Script failed to run :(\n"
+    echo -e "\nScript failed to run :(\n"
 fi
 
-echo "Running LV3.py..."
+echo -e "Running LV3.py...\n"
 python3 LV3.py
 if [ $? ]
 then
-    echo -e "Script ran successfully!\n"
+    echo -e "\nScript ran successfully!\n"
 else
-    echo -e "Script failed to run :(\n"
+    echo -e "\nScript failed to run :(\n"
 fi
 
-echo "Running LV4.py..."
+echo -e "Running LV4.py...\n"
 python3 LV4.py
 if [ $? ]
 then
-    echo -e "Script ran successfully!\n"
+    echo -e "\nScript ran successfully!\n"
 else
     echo -e "Script failed to run :(\n"
 fi
@@ -52,4 +55,6 @@ python3 -m cProfile LV4.py >> LV4.txt && sed '5q;d' LV4.txt >> ../results/profil
 rm LV1.txt LV2.txt LV3.txt LV4.txt
 
 #Print results to screen
-sed 's/^[ \t]*//;s/[ \t]*$/ /' < ../results/profiling.txt | sed '$!N;s/\n//'
+sed 's/^[ \t]*//;s/[ \t]*$/ /' < ../results/profiling.txt | sed '$!N;s/\n//' 
+# first sed command strips all whitespaces from the beginning of each line in the file
+# second sed command combines every two consecutive lines together
